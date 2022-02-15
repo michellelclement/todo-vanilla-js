4b0d1a73f24f71b53825524ec15ada5cb3904f5c
Michelle Clement <michellelclement@gmail.com>
2022-02-15 10:33:40 +0000

    Completed mark button

--- app.js
+++ app.js
@@ -44,4 +44,10 @@ function deleteCheck(e) {
         const todo = item.parentElement;
         todo.remove();
     }
+
+    //Check mark
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed')
+    }
 }
